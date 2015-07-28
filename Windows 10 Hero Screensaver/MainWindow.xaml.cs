33a7bb5749ce07ca8b3977adadb5ec2289118667
ADeltaX <info@adeltax.com>
2015-07-29 00:48:30 +0200

    First version
    First version

---
+++ Windows 10 Hero Screensaver/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoScreensaver {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private bool preview;
+        private Point? lastMousePosition = null;  // Workaround for "MouseMove always fires when maximized" bug.
+        private double volume {
+            get { return FullScreenMedia.Volume; }
+            set {
+            }
+        }
+
+        public MainWindow(bool preview) {
+            InitializeComponent();
+            this.preview = preview;
+            FullScreenMedia.Volume = 1;
+            FullScreenMedia.Stretch = Stretch.Uniform;
+            if (preview) {
+                ShowError("Preview");
+            }
+        }
+
+        private void ScrKeyDown(object sender, KeyEventArgs e) {
+            switch (e.Key) {
+                case Key.Up:
+                case Key.VolumeUp:
+                    volume += 0.1;
+                    break;
+                case Key.Down:
+                case Key.VolumeDown:
+                    volume -= 0.1;
+                    break;
+                case Key.VolumeMute:
+                case Key.D0:
+                    volume = 0;
+                    break;
+                default:
+                    EndFullScreensaver();
+                    break;
+            }
+        }
+
+        private void ScrMouseWheel(object sender, MouseWheelEventArgs e) {
+            volume += e.Delta / 1000.0;
+        }
+
+        private void ScrMouseMove(object sender, MouseEventArgs e) {
+            // Workaround for bug in WPF.
+            Point mousePosition = e.GetPosition(this);
+            if (lastMousePosition != null && mousePosition != lastMousePosition) {
+                EndFullScreensaver();
+            }
+            lastMousePosition = mousePosition;
+        }
+
+        private void ScrMouseDown(object sender, MouseButtonEventArgs e) {
+            EndFullScreensaver();
+        }
+
+        private void ScrSizeChange(object sender, SizeChangedEventArgs e) {
+            FullScreenMedia.Width = e.NewSize.Width;
+            FullScreenMedia.Height = e.NewSize.Height;
+        }
+
+        // End the screensaver only if running in full screen. No-op in preview mode.
+        private void EndFullScreensaver() {
+            if (!preview) {
+                Close();
+            }
+        }
+
+        string FOLDER_VIDEO = Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\W10Media\";
+        string NAME_VIDEO = "W10HeroSilent.mp4";
+
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            if (System.IO.File.Exists(FOLDER_VIDEO + NAME_VIDEO) == false) {
+                ShowError("Video not found. Did you copy the folder containing the video in Windows folder?");
+            } else {
+                FullScreenMedia.Source = new System.Uri(FOLDER_VIDEO + NAME_VIDEO);
+            }
+        }
+
+        private void ShowError(string errorMessage) {
+            ErrorText.Text = errorMessage;
+            ErrorText.Visibility = System.Windows.Visibility.Visible;
+            if (preview) {
+                ErrorText.FontSize = 12;
+            }
+        }
+
+        private void MediaEnded(object sender, RoutedEventArgs e) {
+            FullScreenMedia.Position = new TimeSpan(0);
+        }
+    }
+}
